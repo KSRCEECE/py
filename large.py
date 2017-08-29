f9b8b19a0d786c51d6cfe3e95cc05b542a9c3858
KSRCEECE <30744194+KSRCEECE@users.noreply.github.com>
2017-08-29 13:50:37 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,13 @@
+print("Enter the Number1")
+a= input()
+print("Enter the Number2")
+b= input()
+print("Enter the Number3")
+c= input()
+if (a>b and a>c):
+    print(a," is Largest")
+elif (b>a and b>c):
+    print(b,"  is Largest")
+else:
+     print(c,"  is Largest")
+    
